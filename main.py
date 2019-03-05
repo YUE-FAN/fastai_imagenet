cc83b5435b622e959497822dd9a1c146d31bd8ad
Yue Fan <yuefan@ws66lx.lsv.uni-saarland.de>
2019-03-05 23:47:25 +0100

    add multi-process files .gitignore

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ from fastai.distributed import *
 
 from vggfy import VGG16
 
-
+from fastai.distributed import *
+import argparse
 torch.backends.cudnn.benchmark = True
+parser = argparse.ArgumentParser()
+parser.add_argument("--local_rank", type=int)
+args = parser.parse_args()
+torch.cuda.set_device(args.local_rank)
+torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
+def center_crop(size:int):
+    return [crop(size=size)]
+
+
+
+
 print(num_cpus())
 print("sadsadsad")
 path = untar_data(URLs.CIFAR, dest="./data/")
@@ -15,12 +28,15 @@ ds_tfms = ([*rand_pad(4, 32), flip_lr(p=0.5)], [*center_crop(32)])
 # ds_tfms = None
 # n_gpus = 4
 data = ImageDataBunch.from_folder(path, valid='test', ds_tfms=ds_tfms,  bs=512, num_workers=6).normalize(cifar_stats)
-learn = Learner(data, Resnet50(0, 10, True, 99),  metrics=[accuracy, top_k_accuracy])
+
 # learn = Learner(data, resnet50(), metrics=accuracy)
-learn.distributed(0)
+
+learn = Learner(data, Resnet50(0, 10, True, 99), metrics=[accuracy, top_k_accuracy]).distributed(args.local_rank)
+learn.to_fp16()
+# learn.model = nn.parallel.DistributedDataParallel(learn.model)
 # learn.model = nn.DataParallel(learn.model)
 # print(learn.summary())
-learn.to_fp16()
+
 
 
 print('start training...')
