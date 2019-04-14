2468890b5f3df7a408cd2a36058af6164cc68262
Marius Mosbach <mmosbach@loki.cs.uni-saarland.de>
2019-04-14 08:10:01 +0200

    add vgg16.sh

---
+++ vgg161d.sh
@@ -0,0 +1,27 @@
+python main_torch.py -a vgg16_1d --layer 11 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_1153_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 12 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_1253_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 21 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_2153_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 22 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_2253_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 31 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_3153_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 32 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_3253_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 33 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_3353_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 41 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_4153_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 42 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_4253_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 43 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_4353_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 51 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_5153_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 52 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_5253_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 53 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_5353_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a vgg16_1d --layer 99 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/vgg161d_9953_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
