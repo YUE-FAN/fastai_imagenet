a45d3edbf516ae2f30f1d96fff12b35b5e16a7f3
Marius Mosbach <mmosbach@thor.cs.uni-saarland.de>
2019-04-14 08:13:27 +0200

    add resnet50.sh

---
+++ d1_resnet50.sh
@@ -0,0 +1,29 @@
+python main_torch.py -a d1_resnet50 --layer 11 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_1142_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 12 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_1242_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 20 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_2042_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 21 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_2142_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 22 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_2242_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 23 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_2342_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 30 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3042_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 31 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3142_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 32 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3242_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 33 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3342_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 34 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3442_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 35 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_3542_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 40 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_4042_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 41 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_4142_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
+
+python main_torch.py -a d1_resnet50 --layer 42 --dataset /datasets/imagenet/ --epochs 100 --schedule 30 60 --gamma 0.1 --wd 1e-4 --checkpoint /trained-models/imagenet/resnet501d_4242_100/ --train-batch 256 --multiprocessing-distributed --dist-url tcp://127.0.0.1:8888 --ngpus_per_node 8 --lr 0.1 --workers 32
