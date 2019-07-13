ab489576382084c4a4df64796f6a5268ecb08c3b
Yue Fan <yfan@srv-12-712.mpi-inf.mpg.de>
2019-07-13 12:26:44 +0200

    add resnet1521x1gap and resnet152truncated into main_torch

--- main_torch.py
+++ main_torch.py
@@ -16,7 +16,7 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from vggfy import VGG16, VGG16_1d
-from resnetfy import Resnet50, Resnet50_1d, Resnet152_1d, Resnet50_1x1, Resnet152_1x1
+from resnetfy import Resnet50, Resnet50_1d, Resnet152_1d, Resnet50_1x1, Resnet152_1x1, Resnet152_1x1GAP, Resnet152_truncated
 
 from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
 
@@ -148,6 +148,10 @@ def main_worker(gpu, ngpus_per_node, args):
         model = Resnet50(args.drop, num_classes, True, args.layer)
     elif args.arch.endswith('resnet152_1x1'):
         model = Resnet152_1x1(args.drop, num_classes, True, args.layer)
+    elif args.arch.endswith('resnet152_1x1gap'):
+        model = Resnet152_1x1GAP(args.drop, num_classes, True, args.layer)
+    elif args.arch.endswith('resnet152_truncated'):
+        model = Resnet152_truncated(args.drop, num_classes, True, args.layer)
     elif args.arch.endswith('vgg16'):
         model = VGG16(args.drop, num_classes, True)
     elif args.arch.endswith('vgg16_1d'):
