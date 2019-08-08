136480a7bd6c2eb08d06fdb6a3af7f4ccfd7cabd
YUE-FAN <35504790+YUE-FAN@users.noreply.github.com>
2019-08-08 16:05:49 +0200

    add vgg161x1lmp and vgg161x1lap

--- imagenet_d2.py
+++ imagenet_d2.py
@@ -15,7 +15,7 @@ import torch.optim as optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from vggfy import VGG16, VGG16_1d
+from vggfy import VGG16, VGG16_1d, VGG16_1x1LMP, VGG16_1x1LAP
 from resnetfy import Resnet50, Resnet50_1d, Resnet152_1d, Resnet50_1x1, Resnet152_1x1, Resnet152_1x1LAP, Resnet152_truncated, Resnet152_1x1LMP
 from resnetfy import Resnet50_1x1LMP, Resnet50_1x1LAP, Resnet50_truncated
 
@@ -151,6 +151,10 @@ def main_worker(gpu, ngpus_per_node, args):
         model = Resnet152_1x1(args.drop, num_classes, True, args.layer)
     elif args.arch.endswith('vgg16'):
         model = VGG16(args.drop, num_classes, True)
+    elif args.arch.endswith('vgg16_1x1lmp'):
+        model = VGG16_1x1LMP(args.drop, num_classes, True, args.layer)
+    elif args.arch.endswith('vgg16_1x1lap'):
+        model = VGG16_1x1LAP(args.drop, num_classes, True, args.layer)
     elif args.arch.endswith('vgg16_1d'):
         model = VGG16_1d(args.drop, num_classes, True, args.layer)
     elif args.arch.endswith('d1_resnet50'):
